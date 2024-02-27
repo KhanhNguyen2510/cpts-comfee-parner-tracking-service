5dfab29a9afb007fc346be93a21cc4f043f16ec0
ISC-nguyendqk <nguyendqk@fpt.com>
2024-02-27 17:32:20 +0700

    [Setting] - build new srouce

---
+++ Infrastructure/Data/EFs/CPtsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.EFs
+{
+    public partial class CPtsDbContext : DbContext
+    {
+        public CPtsDbContext() { }
+
+        public CPtsDbContext(DbContextOptions<CPtsDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
---
+++ Infrastructure/Data/EFs/CPtsDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.EFs
+{
+    internal class CPtsDbContextFactory : IDesignTimeDbContextFactory<CPtsDbContext>
+    {
+        public CPtsDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json")
+                  .Build();
+
+            var connectionString = configurationRoot.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<CPtsDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+            return new CPtsDbContext(optionsBuilder.Options);
+        }
+    }
+}
